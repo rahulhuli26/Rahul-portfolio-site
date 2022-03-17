09df28157110f508e51b9a74e3be4984c050542c
Premkumar-hulikoppe <premuhulikoppe@gmail.com>
2022-03-17 18:17:31 +0530

    Updated Profile

--- assests/js/script.js
+++ assests/js/script.js
@@ -41,7 +41,7 @@ $(document).ready(function(){
 document.addEventListener('visibilitychange',
 function(){
     if(document.visibilityState === "visible"){
-        document.title = "Portfolio | Premkumar Hulikoppe";
+        document.title = "Portfolio | Premkumar";
         $("#favicon").attr("href","assests/images/favicon.png");
     }
     else {
